44e3c0d9730b186ea5a0420a15b50e8684f7f84f
manojdm <manumanoj709@gmail.com>
2024-03-10 22:44:13 +0530

    chore: cleaning up stuff

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,13 +3,12 @@ import { Breadcrumb } from "../breadcrumb";
 import { Menu } from "../menu";
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <div className="">
-      <Menu />
-      <div className="p-4 bg-zinc-100">
-        <Breadcrumb />
-        <div>{children}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="">
+			<div className="p-4 bg-zinc-100">
+				<Breadcrumb />
+				<div>{children}</div>
+			</div>
+		</div>
+	);
 };
